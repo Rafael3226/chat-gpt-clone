a03782d1ed4d0cedab2e09ebd54ae364e0924d0c
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-05-11 20:18:30 -0400

    Add Api Key Input

--- script.js
+++ script.js
@@ -5,27 +5,40 @@ const API_KEY = "";
 const chatInput = document.querySelector('.chat-input input[type="text"]');
 const sendButton = document.querySelector(".chat-input button");
 const chatMessages = document.querySelector(".chat-messages ul");
-
+const apiKeyInput = document.querySelector("#api-key");
 // Add event listener to send button
 sendButton.addEventListener("click", sendMessage);
+
+const tags = {
+  error: "error",
+  sent: "sent",
+  received: "received",
+};
 
 async function sendMessage() {
   // Get the message text from the chat input
   const messageText = chatInput.value;
-  addMessage({ text: messageText, sender: "Me", isSent: true });
-  const data = await fetchData(messageText);
-  const responseText = data.choices[0].text;
-  addMessage({ text: responseText, sender: "OPEN AI", isSent: false });
-
   // Clear the chat input after sending the message
   chatInput.value = "";
+  addMessage({ text: messageText, sender: "Me", tag: tags.sent });
+  const data = await fetchData(messageText);
+  if (data.error) {
+    addMessage({ text: data.error.message, sender: "Error", tag: tags.error });
+  } else {
+    const responseText = data.choices[0].text;
+    addMessage({
+      text: responseText,
+      sender: "OPEN AI",
+      tag: tags.received,
+    });
+  }
 }
 
 async function fetchData(prompt) {
   const response = await fetch(OPEN_AI_API, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${API_KEY}`,
+      Authorization: `Bearer ${apiKeyInput.value}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
@@ -36,14 +49,14 @@ async function fetchData(prompt) {
     }),
   });
   const data = await response.json();
+  debugger;
   return data;
 }
 
-function addMessage({ text, sender, isSent }) {
-  const label = isSent ? "sent" : "received";
+function addMessage({ text, sender, tag }) {
   // Add the new message to the chat messages container
   const newMessageLi = document.createElement("li");
-  newMessageLi.classList.add("message", label);
+  newMessageLi.classList.add("message", tag);
   newMessageLi.innerHTML = `
     <span class="sender">${sender}:</span>
     <span class="text">${text}</span>
